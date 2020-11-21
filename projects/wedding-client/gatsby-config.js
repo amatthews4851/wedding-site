50d1b1d0be4c847e0069144c74ea8e0f763c5c4e
Alex Matthews <alexmatthews@darbys-mbp.attlocal.net>
2020-11-20 21:55:58 -0600

    Adds styling

--- projects/wedding-client/gatsby-config.js
+++ projects/wedding-client/gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Parisienne`],
+        display: "swap",
+      },
+    },
+  ],
+};
--- projects/wedding-client/src/pages/index.tsx
+++ projects/wedding-client/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
 import NavBar from "react-bootstrap/NavBar";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
   return (
@@ -15,19 +17,38 @@ export default function Home() {
         backgroundSize: "cover",
       }}
     >
-      <NavBar bg="dark" variant="dark">
+      <NavBar bg="primary" variant="dark">
         <NavBar.Brand>❤️ Mr. & Mrs. Matthews</NavBar.Brand>
       </NavBar>
       <Container
         style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
           paddingTop: "5rem",
+          maxWidth: 500,
         }}
       >
-        Join Us
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "rgba(255, 255, 255, 0.4)",
+            borderRadius: 5,
+            backdropFilter: "blur(4px)",
+            padding: "1em",
+          }}
+        >
+          <h1>Join Us</h1>
+          <h4>October 30, 2021</h4>
+          <h5 style={{ textAlign: "center", fontFamily: "serif" }}>
+            For the union of Mr. Russell Alexander Matthews & Ms. Darby LeeAnn
+            Young
+          </h5>
+          <br />
+          <Button variant="primary" size="lg">
+            RSVP Now
+          </Button>
+        </div>
       </Container>
     </div>
   );
